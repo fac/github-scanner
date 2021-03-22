1fd5a0f6402548fa4bae89299e6beab80d911479
Mark Pitchless <markpitchless@gmail.com>
2021-03-22 15:43:50 +0000

    Re-work query loading
    Start re-work of use of the client. Keep closer the JSON. All based on
    the JSON response, hiding the clients wrapping and name mangling, which
    is just confusing, especially when testing in the GH explorer.
    
    Pull queries out to files for loading. Easier to edit in seperate file
    (tooling) and much easier to move back and forward with the explorer.
    
    Prototype Query class to handle running queries, getting, paging data
    as JSON and insulation from client details.
    
    Make --json an option to any action, instead of a seperate (hacky debug)
    action.
    
    Scan class handles archived and all as options on the class.

--- lib/github/scanner.rb
+++ lib/github/scanner.rb
@@ -8,69 +8,65 @@ module Scanner
 
   class Error < StandardError; end
 
-  RepoQuery = QL.parse <<-GRAPHQL
-    query ($repoEndCursor: String, $repoFirst: Int=10, $filePath: String="Jenkinsfile") {
-      result: organization(login: "fac") {
-        repositories(first: $repoFirst, after: $repoEndCursor) {
-          pageInfo {
-            hasNextPage
-            endCursor
-          }
-          totalCount
-          nodes {
-              name
-              nameWithOwner
-              isArchived
-              defaultBranch: defaultBranchRef {
-              target {
-                  ... on Commit {
-                  authoredDate
-                  author {
-                      name
-                      email
-                  }
-                  oid
-                  file(path: $filePath) {
-                      name
-                      path
-                      type
-                      object {
-                      ... on Blob {
-                          id
-                          text
-                      }
-                    }
-                  }
-                }
-              }
-              name
-            }
-          }
-        }
-      }
-    }
-  GRAPHQL
+  def self.scan(name)
+    case name
+    when :repo;      Scan.new()
+    when :repo_file; Scan.new(query_file: "repo-file.graphql")
+    else
+      raise Error("Unknown scan: #{name}")
+    end
+  end
 
-  def self.new(...)
-    Scan.new(...)
+  def self.total_repos
+    q = GitHub::Scanner::QL.parse file: "repo-names.graphql"
+    q.run(repoFirst: 1).data.dig 'org', 'repos', 'totalCount'
   end
 
   class Scan
-    def repo_query(**vars)
-      QL.query(RepoQuery, variables: vars)
+    attr_accessor :all, :archived
+
+    attr_reader :scanned, :matched
+
+    def initialize(query_file: "repo.graphql", all: false, archived: false)
+      @filters  = []
+      @all      = all
+      @archived = archived
+      add_filter { |r| @all || r['isArchived'] == @archived }
+
+      @query = QL.parse file: query_file
     end
 
-    def repositories(variables={})
+    def run(vars={})
+      @scanned = 0
+      @matched = 0
+
       Enumerator.new do |yielder|
-        repos = QL.with_pagination(
-          RepoQuery, :repositories, 'repoEndCursor', variables)
+        repos = @query.run(vars).paginate('result', 'repositories', after: 'repoEndCursor')
         repos.each do |repo|
+          @scanned += 1
+          next unless repo_filter(repo)
+          @matched += 1
           yielder << repo
+        rescue => err
+          STDERR.puts "ERROR: #{err}"
+          STDERR.puts JSON.pretty_generate(repo)
+          raise err
         end
       end
     end
 
-    alias :repos :repositories
+    def add_filter(&filt)
+      @filters.push filt
+    end
+
+    def repo_filter(repo)
+      @filters.each { |f| return false unless f.call(repo) }
+      true
+    end
+
+    def inflate(res)
+      # Inflate up to a hash repo.name:"", file.path:""
+    end
   end
 
 end # Scanner
--- lib/github/scanner/ql.rb
+++ lib/github/scanner/ql.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'tmpdir'
+require 'digest'
 
 require 'graphql/client'
 require 'graphql/client/http'
@@ -10,19 +11,30 @@ module Scanner
 
 # Query the GitHub GraphQL API at api.github.com/graphql.
 #
+# While this based on graphql/client, it does it's best to not expose that and
+# keep clients working at a level of string queries (loaded from files) and the
+# results as JSON. Makes it easy to work using the github graph explorer.
+# https://docs.github.com/en/graphql/overview/explorer
+# We avoid the clients mapping of names (ie fooBar => foo_bar) as it is just
+# confusing. We also deal with the annoying requirement to store parsed queries
+# in globals! We may well look to replace the client at some point.
+#
 # query_string = <--EOQL
 # EOQL
-# q   = QL.parse(query_string)
+# q = QL.parse(query_string)
 #
-# res = QL.query(q, {})
-# puts "Total Count: #{res.data.result.repositories.total_count}"
+# q = QL.parse(file: "foo.graphql")
 #
-# QL.with_pagination(:repositories, 'endRepoCursor', {}).each |repo| do
-#   puts repo.name
+# q.run
+# puts "Total Count: #{q.data['result']['repositories']['totalCount']"
+#
+# q.run(first: 23).paginate('org','repositories', after: 'endRepoCursor').each |repo| do
+#   puts repo['name']
 # end
 #
 # The module knows the details of the schema handling, download with local file
-# caching. Authenticates using a GitHub personnel access token.
+# caching. Authenticates using a GitHub personnel access token set vis ENV in
+# GITHUB_PAT.
 module QL
   SchemaURL = 'https://api.github.com/graphql' 
 
@@ -38,6 +50,8 @@ module QL
   def self.http
     return @http if @http
 
+    # Call this to check for a token. The error gets lost when called via headers below
+    github_pat
     @http = ::GraphQL::Client::HTTP.new(SchemaURL) do
       def headers(context) { "Authorization" => "Bearer #{GitHub::Scanner::QL.github_pat}" }
       end
@@ -61,26 +75,71 @@ module QL
     FileUtils.rm_rf schema_file
   end
 
-  def self.parse(...)
-    client.parse(...)
+  def self.query_dir(*paths)
+    @query_dir ||= File.join File.expand_path("../../../..", __FILE__), "query"
+    return @query_dir if paths.empty?
+    File.join @query_dir, paths
   end
 
+  # parse(qstr, name: "")
+  # parse(file: "", name: "")
+  def self.parse(qstr = "", file: nil, name: nil)
+    if qstr.empty? && !file.nil?
+      file = query_dir(file) if Pathname.new(file).relative?
+      qstr = File.read file
+    end
+
+    # graphql/client insists on queries being module constants!
+    cname = "Q_%s" % Digest::MD5.hexdigest(qstr).upcase
+    q = if QL.const_defined? cname.to_s
+      QL.const_get cname
+    else
+      QL.const_set cname, client.parse(qstr)
+    end
+    QueryRunner.new q
+  end
+
+  # TODO: check arg for GraphQL::Client::OperationDefinition auto parse string if not
   def self.query(...)
-    client.query(...)
+    res = client.query(...)
+    res.errors.each { |err| STDERR.puts "ERROR: #{err}" }
+    res
   end
 
-  def self.with_pagination(query, page_on_field, after_var, variables={})
-    Enumerator.new do |yielder|
-      response = query(query, variables: variables).data.result
-      response.send(page_on_field).nodes.each { |node| yielder << node }
+  class QueryRunner
+    extend Forwardable
 
-      page_info = response.send(page_on_field).page_info
-      while page_info.has_next_page != false
-        cursor = page_info.end_cursor
-        variables[after_var] = cursor
-        response = query(query, variables: variables).data.result
-        response.send(page_on_field).nodes.each { |node| yielder << node }
-        page_info = response.send(page_on_field).page_info
+    attr_reader :query, :vars, :data
+
+    def_delegators :@data, :to_h, :to_json, :key
+
+    def initialize(q, vars = {})
+      @query      = q
+      @variables  = vars
+      @data       = nil
+    end
+
+    def run(vars = {})
+      @variables = vars
+      @data      = QL.query(@query, variables: vars).data.to_h
+      # TODO: the errors? .errors
+      self
+    end
+
+    # q.run.paginate('result', 'repositories', after: 'repoEndCursor').each do |r|
+    #   puts r['name']
+    # end
+    def paginate(*path, after: "after")
+      vars = @variables.clone
+      Enumerator.new do |yielder|
+        loop do
+          q = @data.dig *path
+          q['nodes'].each { |node| yielder << node }
+
+          break unless q['pageInfo']['hasNextPage']
+          vars[after] = q['pageInfo']['endCursor']
+          run vars
+        end
       end
     end
   end
